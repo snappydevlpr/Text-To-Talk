2834655876f982f3db335e271fd6721ccbc3b9e7
Austin Suarez <austinmsuarez@gmail.com>
2022-03-14 21:37:57 -0500

    speech to text application

--- Text To Talk/ContentView.swift
+++ Text To Talk/ContentView.swift
@@ -6,11 +6,59 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    @State private var statement:String = "Enter Statement";
+    @State private var speechImageState:String = "mute";
+    @State private var playState:Bool = false;
+
     var body: some View {
-        Text("Hello, world!")
+        ZStack{
+            Color("background")
+                .ignoresSafeArea()
+            VStack{
+                TextEditor(text: $statement)
+                   .padding()
+                   .background(Color.white)
+                   .foregroundColor(Color.black)
+                   .frame(width: UIScreen.main.bounds.width-80, height: UIScreen.main.bounds.height-500)
+                   .cornerRadius(25)
+                Spacer()
+                
+                
+                Image(speechImageState)
+                    .resizable()
+                    .frame(width: 100, height: 100, alignment: .center)
+                Spacer()
+                Button(action:{
+                    playState = !playState;
+                    if(playState){
+                        speechImageState = "microphone";
+                    }
+                    else{
+                        speechImageState = "mute";
+                    }
+                    
+                    let utterance = AVSpeechUtterance(string: statement)
+                    utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+                    utterance.rate = 0.5
+
+                    let synthesizer = AVSpeechSynthesizer()
+                    synthesizer.speak(utterance)
+                }){
+                    Text("Text to Talk")
+                }
+                .padding()
+                .foregroundColor(Color.white)
+                .buttonStyle(.bordered)
+                .background(Color("accent"))
+                .clipShape(Capsule())
+            }
+            .ignoresSafeArea()
             .padding()
+        }
+     
     }
 }
 
